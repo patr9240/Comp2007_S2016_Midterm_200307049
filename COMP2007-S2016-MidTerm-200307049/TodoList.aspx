3ee3a336720b81992b5c10da2d426d773225b044
patr9240 <patr9240@gmail.com>
2016-06-23 19:04:27 -0400

    Added ability to sort gridview columns.

--- COMP2007-S2016-MidTerm-200307049/TodoList.aspx
+++ COMP2007-S2016-MidTerm-200307049/TodoList.aspx
@@ -7,7 +7,8 @@
                 <h3>Todo List</h3>
                 <a href="/TodoDetails.aspx" class="btn btn-success btn-sm"><i class="fa fa-plus"></i>Add a Todo</a>
                 <asp:GridView ID="TodoGridView" AllowPaging="true" PageSize="3" runat="server" CssClass="table table-bordered table-striped table-hover"
-                    AutoGenerateColumns="false" DataKeyNames="TodoID" OnRowDeleting="TodoGridView_RowDeleting" OnPageIndexChanging="TodosGridView_PageIndexChanging">
+                    AutoGenerateColumns="false" DataKeyNames="TodoID" OnRowDeleting="TodoGridView_RowDeleting" OnPageIndexChanging="TodosGridView_PageIndexChanging"
+                    AllowSorting="true" OnSorting="TodoGridView_Sorting" OnRowDataBound="TodoGridView_RowDataBound">
                     <PagerStyle CssClass="pagination-ys" />
                     <Columns>
                         <asp:TemplateField HeaderText="No.">
@@ -16,11 +17,10 @@
                             </ItemTemplate>
                         </asp:TemplateField>
 
-                        <asp:BoundField DataField="TodoID" Visible="false" />
-                        <asp:BoundField DataField="TodoName" HeaderText="Name" Visible="true" />
-                        <asp:BoundField DataField="TodoNotes" HeaderText="Notes" Visible="true" />
+                        <asp:BoundField DataField="TodoID" Visible="false" SortExpression="TodoID" />
+                        <asp:BoundField DataField="TodoName" HeaderText="Name" Visible="true" SortExpression="TodoName" />
+                        <asp:BoundField DataField="TodoNotes" HeaderText="Notes" Visible="true" SortExpression="TodoNotes" />
                         <asp:CheckBoxField DataField="Completed" HeaderText="Completed" Visible="true" />
-
                         <asp:HyperLinkField HeaderText="Edit" Text="<i calss='fa fa-encil-square-o fa-lg'></i> Edit"
                             NavigateUrl="/TodoDetails.aspx" ControlStyle-CssClass="btn btn-primary btn-sm" DataNavigateUrlFields="TodoID"
                             runat="server" DataNavigateUrlFormatString="/TodoDetails.aspx?TodoID={0}" />
--- COMP2007-S2016-MidTerm-200307049/TodoList.aspx.cs
+++ COMP2007-S2016-MidTerm-200307049/TodoList.aspx.cs
@@ -24,6 +24,8 @@ namespace COMP2007_S2016_MidTerm_200307049
             //if loading page for the first time, populate the grid
             if (!IsPostBack)
             {
+                Session["SortColumn"] = "TodoID"; //default sort column
+                Session["SortDirection"] = "Ascending"; // default sort direction
                 //get the todo list data
                 this.GetTodos();
             }
@@ -42,12 +44,13 @@ namespace COMP2007_S2016_MidTerm_200307049
             //connect to EF
             using (TodoConnection db = new TodoConnection())
             {
-                //query the Games table using EF and LINQ
+                string SortString = Session["SortColumn"].ToString() + " " + Session["SortDirection"].ToString();
+                //query the Todos table using EF and LINQ
                 var Todos = (from allTodos in db.Todos
                              select allTodos);
 
                 //bind results to gridview
-                TodoGridView.DataSource = Todos.AsQueryable().ToList();
+                TodoGridView.DataSource = Todos.AsQueryable().OrderBy(SortString).ToList();
                 TodoGridView.DataBind();
             }
         }
@@ -124,5 +127,61 @@ namespace COMP2007_S2016_MidTerm_200307049
             this.GetTodos();
         }
 
+        /**
+         * <summary>
+         * This method sorts the todos list gridview
+         * </summary>
+         * @method TodoGridView_Sorting
+         * @returns {void}
+         * */
+        protected void TodoGridView_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            // get the column to sort by
+            Session["SortColumn"] = e.SortExpression;
+
+            
+            //refresh the grid
+            this.GetTodos();
+
+            //toggle the direction from ASC and DSC
+            Session["SortDirection"] = Session["SortDirection"].ToString() == "Ascending" ? "Descending" : "Ascending";
+
+
+        }
+
+        /**
+         * <summary>
+         * This method sorts the todos list gridview
+         * </summary>
+         * @method TodoGridView_RowDataBound
+         * @returns {void}
+         * */
+        protected void TodoGridView_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (IsPostBack)
+            {
+                if (e.Row.RowType == DataControlRowType.Header)//if header row is clicked
+                {
+                    LinkButton linkButton = new LinkButton();
+
+                    for (int index = 0; index < TodoGridView.Columns.Count - 1; index++)
+                    {
+                        if (TodoGridView.Columns[index].SortExpression == Session["SortColumn"].ToString())
+                        {
+                            if (Session["SortDirection"].ToString() == "Ascending")
+                            {
+                                linkButton.Text = "<i class='fa fa-caret-up fa-lg'> </i>";
+                            }
+                            else
+                            {
+                                linkButton.Text = "<i class='fa fa-caret-down fa-lg'> </i>";
+                            }
+
+                            e.Row.Cells[index].Controls.Add(linkButton);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
